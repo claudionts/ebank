37077399e029ed248d409a7862bdbf136fe080a1
claudionts <claudionts@gmai.com>
2022-03-10 16:36:48 -0300

    create test reset file controller

---
+++ test/ebank_web/controllers/reset_file_controller_test.exs
@@ -0,0 +1,15 @@
+defmodule EbankWeb.ResetFileControllerTest do
+  use EbankWeb.ConnCase
+
+  setup %{conn: conn} do
+    {:ok, conn: conn}
+  end
+
+  describe "index /reset router" do
+    test "reset file data mock", %{conn: conn} do
+      assert "OK" = conn
+      |> get(Routes.reset_file_path(conn, :index))
+      |> json_response(200)
+    end
+  end
+end
